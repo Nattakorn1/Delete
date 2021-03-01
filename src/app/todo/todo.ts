def39dd3ced1b4a4e05112d54e8d2f13d78ade42
Nattakorn1 <natta.delltwin@gmail.com>
2021-03-01 20:14:13 +0700

    Delete

---
+++ src/app/todo/todo.ts
@@ -0,0 +1,19 @@
+import { templateJitUrl } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'todo',
+    templateUrl: './todo.html',
+    styleUrls: ['./todo.css']
+})
+export class Todo {
+    tasks : string [] = [] ;
+    task : string ;
+    
+    addTask(){
+        this.tasks.push(this.task);
+    }
+    deleteTask(input:string){
+        this.tasks = this.tasks.filter((t) => t !== input);
+    }
+}
